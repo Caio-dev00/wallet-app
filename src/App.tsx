65b96e8232148d43a6ffa851d396c750ff08511d
marqs_dev <caiohenrique1146@gmail.com>
2023-06-02 18:28:20 -0300

    Finaliazando configurações de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, {useCallback} from "react";
+import * as SplashScreen from 'expo-splash-screen';
 import { View, Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins";
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import COLORS from "../src/styles/theme";
+
+SplashScreen.preventAutoHideAsync();
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Wallet App</Text>
-    </View>
+    <ThemeProvider theme={COLORS}>
+      <View
+        onLayout={onLayoutRootView}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>Wallet App</Text>
+      </View>
+    </ThemeProvider>
   )
 }
 
