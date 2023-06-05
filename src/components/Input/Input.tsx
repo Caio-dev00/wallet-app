883f93e19f6ee1ca78c5e1472b046280cb0d06e8
marqs_dev <caiohenrique1146@gmail.com>
2023-06-05 17:48:33 -0300

    Criando inputs

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./styles";
+import { Text } from "react-native";
+
+const Input: React.FC = () => {
+  return(
+    <Container>
+        <Text>Input</Text>
+    </Container>
+  )
+}
+
+export { Input }
